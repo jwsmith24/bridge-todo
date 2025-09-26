96c16566dbad5e73d1155ded8fe1a8602c175dfb
Jacob Smith <jacob.w.smith97.mil@army.mil>
2025-09-26 11:37:01 -0500

    add lombok annotations to entities

--- spring-todo/src/main/java/dev/jake/springtodo/todo/User.java
+++ spring-todo/src/main/java/dev/jake/springtodo/todo/User.java
@@ -1,5 +1,14 @@
 package dev.jake.springtodo.todo;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class User {
     private Long id;
     private String name;
